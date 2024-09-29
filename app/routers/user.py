8ea69acfede16754790737342f309dc36c81d986
Jacob Grigorev <grigorevakov30@gmail.com>
2024-09-29 03:16:19 +0300

    Upgrade file

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from app.backend.db_depends import get_db
 from typing import Annotated
-from app.models import User
+from app.models import User, Task
 from app.schemas import CreateUser, UpdateUser
 from sqlalchemy import insert, select, update, delete
 from slugify import slugify
+
 
 router = APIRouter(prefix="/user", tags=["user"])
 
@@ -14,6 +15,17 @@ router = APIRouter(prefix="/user", tags=["user"])
 async def all_users(db: Annotated[Session, Depends(get_db)]):
     users = db.scalars(select(User)).all()
     return users
+
+
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    tasks = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    if not tasks:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='No tasks found for this user'
+        )
+    return tasks
 
 
 @router.get('/user_id')
@@ -70,9 +82,11 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
             status_code=status.HTTP_404_NOT_FOUND,
             detail='There is no user found'
         )
+    db.execute(delete(Task).where(Task.user_id == user_id))
     db.execute(delete(User).where(User.id == user_id))
     db.commit()
     return {
         'status_code': status.HTTP_200_OK,
         'transaction': 'User delete is successful!'
     }
+
